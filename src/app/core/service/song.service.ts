59e87c1e4172919c61e2cb2e319b58669c01bf75
Heiko <heiko.piirme@khk.ee>
2019-04-21 20:21:04 +0300

    Väljalogimine, logimine emailiga. Registreerimisel korduva parooli ja emaili korral errorid.

---
+++ src/app/core/service/song.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {ToastrService} from 'ngx-toastr';
+import {Observable} from 'rxjs';
+import {ErrorResponse} from '../model/error';
+import {environment} from '../../../environments/environment';
+import {Song} from '../model/song/song';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SongService {
+
+  constructor(private router: Router, private http: HttpClient, private toastr: ToastrService) { }
+
+  public getActiveSongById(id): Observable<Song> {
+    return new Observable(observer => {
+      this.http.get(environment.API_URL + '/activesongs/' + id).subscribe(
+          (data: Song) => {
+            observer.next(data);
+          },
+          (err: ErrorResponse) => this.handleError(observer, err)
+      );
+    });
+  }
+
+  private handleError(observer, error: ErrorResponse) {
+    observer.error(error);
+  }
+
+}
